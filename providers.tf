6c714eac7aa9f71f7e9e0835ee72c8f829244bc0
Ruwani <ruwani054@gmail.com>
2024-06-02 14:59:27 +0530

    initial comimit

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {}
+}
+
+
+
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "5.52.0"
+#     }
+
+    
+#   }
+# }
+
+# provider "aws" {
+#   # Configuration options
+# }
